fb12aee0c30cee9ff4ac00b41c64e0869e118924
KickNext <nikitiser@gmail.com>
2025-05-14 03:09:19 +0500

    Removes duplicate import statements
    Cleans up redundant imports to improve code readability and organization.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_date_range_picker/flutter_date_range_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_date_range_picker/flutter_date_range_picker.dart';
 
 void main() {
   runApp(const MyApp());
